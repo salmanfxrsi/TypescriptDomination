abe35e22fc9fcf2039ab7dda9ff915a34961370f
salmanfxrsi <salmanxprivate@gmail.com>
2025-03-03 02:10:49 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+"use strict";
+class Student {
+    constructor() {
+        this.name = 'Alex Green';
+        this.roll = 42;
+        this.grade = 5.00;
+    }
+}
+let s1 = new Student();
+console.log(s1.name);
---
+++ app.ts
@@ -0,0 +1,8 @@
+class Student {
+    name = 'Alex Green';
+    roll = 42;
+    grade = 5.00;
+}
+
+let s1 = new Student();
+console.log(s1.name)
